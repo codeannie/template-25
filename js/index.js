8840d28207528c57b45322ccd22bda7d3aa63d94
Annie Chiang <devbear22@gmail.com>
2019-05-08 11:02:38 -0700

    use classes in nav links to show/hide sections

--- js/index.js
+++ js/index.js
@@ -1,25 +1,12 @@
 // // Navigation - show sections
 
-// $(document).ready(() => {
-// 	$('#nav a').on('click', (e) => {
-//     e.preventDefault();
-//     $('.show').removeClass('show');
-//     $( $(this).attr('data-target')).addClass('show');
-// 	})
-// });
-
 function handleNav() {
   $('#nav a').on('click', e => {
-    let id = $(this).attr('id');
-    console.log('id? ->', id)
-    console.log('event data', e)
+    console.log('event data ->', e)
     e.preventDefault();
-    console.log('inner html ->', e.target.innerHTML);
     $('.show').removeClass('show');
-    // refactor this solution
-    // $(e.target).addClass('show');
-    // $(this).addClass('show');
-    $('#' + e.target.innerHTML.toLowerCase()).addClass("show")
+    $('#' + e.target.className).addClass('show');
+    // $('#' + e.target.innerHTML.toLowerCase()).addClass("show")
   });
 }
 
